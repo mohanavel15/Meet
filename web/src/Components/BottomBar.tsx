5688b3e902d7ba61dd0e1021bd4f65d925bf7a47
Mohan <mohanavel15@protonmail.com>
2022-09-05 11:30:18 +0530

    refactoring & typo fix 2

--- web/src/Components/BottomBar.tsx
+++ web/src/Components/BottomBar.tsx
@@ -1,22 +1,17 @@
-import { Accessor, createEffect, createSignal, JSX, Show } from "solid-js"
+import { createEffect, createSignal } from "solid-js"
 import { BiSolidMicrophone, BiSolidMicrophoneOff } from 'solid-icons/bi'
-import { 
-    BsCameraVideoFill, 
-    BsCameraVideoOffFill,
-    BsTelephoneXFill
-} from 'solid-icons/bs'
+import { BsCameraVideoFill, BsTelephoneXFill} from 'solid-icons/bs'
 import State from "../Models/State"
 import ActionButton from "./ActionButton"
 
-interface BottonBarProps {
+interface BottomBarProps {
     state: State
     setMute: (bool: boolean) => void
     setVideo: (bool: boolean) => void
     endCall: () => void
 }
 
-export default function BottonBar(prop: BottonBarProps) {
-    const [self, _] = createSignal(true)
+export default function BottomBar(prop: BottomBarProps) {
     const [video, setvideo] = createSignal(false)
     const [mute, setmute] = createSignal(false)
 
@@ -27,26 +22,25 @@ export default function BottonBar(prop: BottonBarProps) {
 
     return (
     <div class="h-12 bg-slate-900 w-full items-center flex px-8 justify-center">
-        <ActionButton visible={video} when={false} onclick={() => prop.setVideo(true)}>
+        <ActionButton visible={video()} when={false} onclick={() => prop.setVideo(true)}>
         <BsCameraVideoFill size={20} color="#000000" />
         </ActionButton>
         
-        <ActionButton visible={video} when={true} onclick={() => prop.setVideo(false)}>
+        <ActionButton visible={video()} when={true} onclick={() => prop.setVideo(false)}>
         <BsCameraVideoFill size={20} color="#E60000" />
         </ActionButton>
         
-        <ActionButton visible={mute} when={false} onclick={() => prop.setMute(true)}>
+        <ActionButton visible={mute()} when={false} onclick={() => prop.setMute(true)}>
         <BiSolidMicrophone size={20} color="#000000"/>
         </ActionButton>
         
-        <ActionButton visible={mute} when={true} onclick={() => prop.setMute(false)} >
+        <ActionButton visible={mute()} when={true} onclick={() => prop.setMute(false)} >
         <BiSolidMicrophoneOff size={20} color="#E60000"/>
         </ActionButton>
         
-        <ActionButton visible={self} when={true} onclick={prop.endCall}>
+        <ActionButton visible={true} when={true} onclick={prop.endCall}>
         <BsTelephoneXFill size={20} color="#E60000"/>
         </ActionButton>
-        
     </div>
   )
 }
